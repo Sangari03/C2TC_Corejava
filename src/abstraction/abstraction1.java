05dcd0cf1b3dba4979ad294504ddda5441d6bf93
ELCOT <ELCOT@Lenovo>
2024-08-04 10:36:52 +0530

    abstraction1

---
+++ src/abstraction/abstraction1.java
@@ -0,0 +1,31 @@
+package abstraction;
+
+abstract class Animal {
+  // Abstract method (does not have a body)
+  public abstract void animalSound();
+  // Regular method
+  public void sleep() {
+    System.out.println("cse");
+  }
+}
+
+// Subclass (inherit from Animal)
+class Pig extends Animal {
+  public void animalSound() {
+    // The body of animalSound() is provided here
+    System.out.println("sangari ");
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    Pig myPig = new Pig(); // Create a Pig object
+    myPig.animalSound();
+    myPig.sleep();
+  }
+}
+
+
+
+
+
