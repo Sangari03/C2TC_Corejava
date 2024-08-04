26a4dde555bec4f2d88c4df478845bd908a13afc
ELCOT <ELCOT@Lenovo>
2024-08-04 08:39:42 +0530

    sankari

---
+++ src/array/array2.java
@@ -0,0 +1,27 @@
+package array;
+
+public class array2 {
+
+
+
+	public static void main(String[] args) {
+		// declares an Array of integers.
+		array2[] arr;
+		// allocating memory for 5 objects of type Student.
+		arr = new array2[5];
+		// initialize the first elements of the array
+		arr[0] = new array2();
+		// initialize the second elements of the array
+		arr[1] = new array2();
+		// so on...
+		arr[2] = new array2();
+		arr[3] = new array2();
+		arr[4] = new array2();
+		
+		// accessing the elements of the specified array
+		for (int i = 0; i < arr.length; i++)
+			System.out.println();
+	}
+
+}
+
