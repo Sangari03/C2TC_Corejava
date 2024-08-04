200c08124aa0422aac9860e2c4aa1b167af3b0ef
ELCOT <ELCOT@Lenovo>
2024-08-04 20:00:03 +0530

    mlarray

--- src/array/MLArray.java
+++ src/array/MLArray.java
@@ -1,24 +1,12 @@
 package array;
 
-public class MLArray {//Program to demonstrate Multidimensional Array
-
-
-	public class MultiDimArrayDemo {
-		public static void main(String[] args) {
-
-			int c[][] = { { 12, 45 }, { 10, 40 }, { 5, 7 } }; // new int[3][2];
-
-			System.out.println("No. of Rows in C array : " + c.length);
-
-			MLArray.printArray(c);
-		}
-
-	}
-
-public static void printArray(int[][] c) {
-	// TODO Auto-generated method stub
-	
-}
-
-
+public class MLArray {
+  public static void printArray(int[][] c) {
+    System.out.println("Array Elements are as follows:");
+    for (int i = 0; i < c.length; i++) {
+      for (int j = 0; j < c[i].length; j++)
+        System.out.print(c[i][j] + " ");
+      System.out.println();
+    }
+  }
 }
